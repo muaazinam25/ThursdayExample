46c421527cc49ed5fb72dff61390f423a1042f26
muaazinam25 <muhammua@sheridancollege.ca>
2020-05-28 10:14:04 -0400

    It is my first commit procedure.

---
+++ src/softwaredesignex1/SoftwareDesignEx1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwaredesignex1;
+
+/**
+ *
+ * @author muaaz
+ */
+public class SoftwareDesignEx1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Test1");
+    }
+    
+}
